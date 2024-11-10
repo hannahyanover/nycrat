e297a44988b456cc28a34e573c85a11903790739
hannahyanover <154846698+hannahyanover@users.noreply.github.com>
2024-11-10 15:03:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify, render_template_string
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response, jsonify, render_template_string
 
-app = Flask(__name__)
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
 
 html_template = """
 <!DOCTYPE html>
@@ -41,13 +45,52 @@ html_template = """
 </html>
 """
 
+DB_USER = "zz3306"
+DB_PASSWORD = "hry2106"
+
+DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
+
+DATABASEURI = "postgresql://zz3306:hry2106@w4111.cisxo09blonu.us-east-1.rds.amazonaws.com/w4111"
+
+@app.before_request
+def before_request():
+  """
+  This function is run at the beginning of every web request 
+  (every time you enter an address in the web browser).
+  We use it to setup a database connection that can be used throughout the request
+
+  The variable g is globally accessible
+  """
+  try:
+    g.conn = engine.connect()
+  except:
+    print("uh oh, problem connecting to database")
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  """
+  At the end of the web request, this makes sure to close the database connection.
+  If you don't the database could run out of memory!
+  """
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+
+
 @app.route('/')
 def home():
     return render_template_string(html_template)
 
 @app.route('/sighting')
 def sighting():
-    return "Personal Rat Sighting Page (Coming soon)"
+    cursor = g.conn.execute("SELECT comment FROM personal_rat_sighting")
+  comments = []
+  for result in cursor:
+    comments.append(result['comment'])  # can also be accessed using result[0]
+  cursor.close()
 
 @app.route('/report')
 def report():
